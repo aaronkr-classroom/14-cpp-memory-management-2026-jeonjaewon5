b503c0f559703139e0470258f1538b1233fa7f6c
ththfql <145094239+ththfql@users.noreply.github.com>
2026-06-10 17:12:32 +0900

    chap14

---
+++ chapter13/Grad.h
@@ -0,0 +1,25 @@
+#pragma once
+// Grad.h
+
+#ifndef GUARD_Grad_h
+#define GUARD_Grad_h
+
+#include <iostream>
+
+#include "Core.h"
+
+using namespace std;
+
+class Grad : public Core {
+public:
+	Grad() : thesis(0) { };
+	Grad(istream& is) { read(is); };
+	double grade() const;
+	istream& read(istream&);
+
+private:
+	double thesis; // Á¹¾÷ ³í¹®
+	Grad* clone() const { return new Grad(*this); }
+};
+
+#endif
